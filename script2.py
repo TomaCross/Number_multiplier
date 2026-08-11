ee3525c3c76d47e9f2a749452ca029f9455d695b
Toma_Cross <deivharison@gmail.com>
2026-08-11 21:31:35 +0400

    Add files via upload

---
+++ script2.py
@@ -0,0 +1,6 @@
+
+num1 = int(input('ведите первое число='))
+num2 = int(input('ведите второе число='))
+num3 = int(input('ведите третье число='))
+num4 = int(input('ведите шетыре число='))
+print(num1 * num2 * num3 * num4)
